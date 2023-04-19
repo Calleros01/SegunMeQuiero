8e372bbf42307ac98835207225632b8ad90455ca
JuampooXD <125276755+Calleros01@users.noreply.github.com>
2023-04-19 07:46:55 -0600

    2023_03_22_003_INOUT_V1.
    el de los tacos

---
+++ 2023_03_22_003_INOUT_V1/2023_03_22_003_INOUT_V1.cpp
@@ -0,0 +1,47 @@
+// 2023_03_22_003_INOUT_V1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// Juan Pablo Calleros
+// En este programa aprenderemos a usar bibliotecas
+// Recibiremos datos del usuario
+
+#include <iostream>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    std::cout << "Hola bienvenido al programa";
+    bool pregunta = false;
+    int n_tacos = 5;
+    float costo = 16;
+    char preciototal;
+    std::string;
+
+    std::cout << "Lea y responda con verdadero o falso \n " <<
+        "Anoche cene tacos al pastor? 0)No 1)Si" << std::endl;
+    //Recibir respuesta del usuario
+    std::cin >> pregunta;
+
+    std::cout << "Cuantos tacos? " << std::endl;
+    std::cout << "Ya veo entonces es " << pregunta << " que cenaste tacos" <<
+        std::endl;
+    std::cin >> n_tacos;
+ ;
+        std::cout << "cuanto salio cada taco? \n";
+        std::cout << costo; 16;
+    std::cout << "\nbastante carito no bro? \n";
+
+        std::cout << "que tal estuvieron los tacos? \n";
+        std::cout << "lo normal \n";
+        std::cout << "y de menos le podias echar lo que quisieras?";
+
+
+    
+   
+    
+
+    
+
+    
+
+}
+
